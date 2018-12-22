cd96d17860b6fd9725799b923933c79f2ff79c0d
Loredana Lungu <loredana.lungu@tradeshift.com>
2018-12-22 17:00:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    inputedValue:''
+  };
+ handleChange = (event) => {
+   const that = this;
+   const thisElem = event.target;
+   that.setState({
+        inputedValue: thisElem.value
+   })
+}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" value={this.state.inputedValue} onChange={this.handleChange}/>
+          <p className="echo">Echo:</p>
+          <p>{this.state.inputedValue }</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
